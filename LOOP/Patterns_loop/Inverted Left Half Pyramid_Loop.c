ecc271d42eb7bf3b145b356061a2ee541dc192fb
dhruvpatel012 <swaminarayan0112@gmail.com>
2026-01-07 16:16:35 +0530

    Update Inverted Left Half Pyramid_Loop.c

--- LOOP/Patterns_loop/Inverted Left Half Pyramid_Loop.c
+++ LOOP/Patterns_loop/Inverted Left Half Pyramid_Loop.c
@@ -4,17 +4,24 @@ int main(){
 	
 	int i,j,k;
 	
-	for(i=1;i<=5;i++){
-		
-		for(j=5;j>=i;j--){
-			printf("*");
-		}
+	for(i=5;i>=1;i--){
 		
 		for(k = 1; k<=5-i; k++){
 			printf(" ");
 		}
 		
+		for(j=1; j<=i; j++){
+			printf("*");
+		}
+		
+		
 		printf("\n");
 	}
 	return 0;
 }
+
+// * * * * *
+// * * * * 
+// * * * 
+// * * 
+// *
